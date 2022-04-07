f58481b32ba7eceb96fa0e65138a1a57d699b292
Robert Campbell <waprave@gmail.com>
2022-04-07 11:14:09 -0400

    Updated xml

--- BulkanGen/BulkanGen/Helpers.cs
+++ BulkanGen/BulkanGen/Helpers.cs
@@ -273,6 +273,13 @@ namespace BulkanGen
                 case "StdVideoEncodeH265SliceHeader":
                 case "StdVideoEncodeH265ReferenceInfo":
                 case "StdVideoEncodeH265ReferenceModifications":
+                // bulkan
+                case "StdVideoH264Level":
+                case "StdVideoH265Level":
+                case "StdVideoEncodeH264ReferenceInfo":
+                case "StdVideoEncodeH264RefMemMgmtCtrlOperations":
+                case "StdVideoEncodeH265SliceSegmentHeader":
+                    // end bulkan
                     return true;
                 default:
                     return false;
